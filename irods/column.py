b298e347e70419705f50257a05373b85d414db54
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-03-14 22:00:48 -0700

    Added Criterion class

--- irods/column.py
+++ irods/column.py
@@ -4,6 +4,30 @@ class Column(object):
         self.icat_key = icat_key
         self.icat_id = icat_id
 
+    def __lt__(self, other):
+        return Criterion('<', self, other)
+
+    def __le__(self, other):
+        return Criterion('<=', self, other)
+
+    def __eq__(self, other):
+        return Criterion('=', self, other)
+
+    def __ne__(self, other):
+        return Criterion('!=', self, other)
+
+    def __gt__(self, other):
+        return Criterion('>', self, other)
+
+    def __ge__(self, other):
+        return Criterion('>=', self, other)
+
+class Criterion(object):
+    def __init__(self, op, col, value):
+        self.op = op
+        self.column = col
+        self.value = value
+        
 class ColumnType(object):
     def to_python(self):
         pass
