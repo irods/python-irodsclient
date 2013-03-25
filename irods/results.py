38c192c05dbba448d68fffc12eb186183830454d
Chris La Rose <cjlarose@gmail.com>
2013-03-24 17:24:50 -0700

    Formatting headers of result set string representation

--- irods/results.py
+++ irods/results.py
@@ -7,8 +7,8 @@ class ResultSet(object):
         self.rows = [self._format_row(i) for i in range(self.length)]
 
     def __str__(self):
-        columns = [(col, max(len(str(col.attribute_index)), max([len(x) for x in col.values]))) for col in self._raw.sql_results]
-        header = " | ".join([str(col.attribute_index).ljust(width) for (col, width) in columns])
+        columns = [(col, max(len(str(ModelBase.columns[col.attribute_index].icat_key)), max([len(x) for x in col.values]))) for col in self._raw.sql_results]
+        header = " | ".join([str(ModelBase.columns[col.attribute_index].icat_key).ljust(width) for (col, width) in columns])
         rows = "\n".join([" | ".join( [columns[j][0].values[i].ljust(columns[j][1]) for j in range(len(columns)) ]) for i in range(self._raw.row_count) ])
         return header + "\n" + rows + "\n"
 
