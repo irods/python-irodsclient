9d7438295a8109d6e67f9ac72ce6ca5fcc8b7667
Chris La Rose <cjlarose@gmail.com>
2013-03-28 10:48:41 -0700

    Added test for startuppack

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import unittest
 from irods.message import StartupPack, AuthResponseInp, InxIvalPair, \
-InxValPair, KeyValPair, GenQueryInp, SqlResult, GenQueryOut
+InxValPair, KeyValPair, GenQueryInp, SqlResult
+from xml.etree import ElementTree as ET
 
 class TestMessages(unittest.TestCase):
     
@@ -15,11 +16,32 @@ class TestMessages(unittest.TestCase):
         sup.clientRcatZone = "yoyoyo"
         sup.relVersion = "irods3.2"
         sup.apiVersion = "d"
-        sup.option = 4
+        sup.option = "hellO"
         xml_str = sup.pack()
-        print xml_str
-        expected_xml = "hello"
-        self.assertEqual(xml_str, expected_xml)
+        expected = "<StartupPack_PI>\
+<irodsProt>2</irodsProt>\
+<reconnFlag>3</reconnFlag>\
+<proxyUser>rods</proxyUser>\
+<proxyRcatZone>tempZone</proxyRcatZone>\
+<clientUser>rods</clientUser>\
+<clientRcatZone>yoyoyo</clientRcatZone>\
+<relVersion>irods3.2</relVersion>\
+<apiVersion>d</apiVersion>\
+<option>hellO</option>\
+</StartupPack_PI>"
+        self.assertEqual(xml_str, expected)
+
+        sup2 = StartupPack()
+        sup2.unpack(ET.fromstring(expected))
+        self.assertEquals(sup2.irodsProt, 2)
+        self.assertEquals(sup2.reconnFlag, 3)
+        self.assertEquals(sup2.proxyUser, "rods")
+        self.assertEquals(sup2.proxyRcatZone, "tempZone")
+        self.assertEquals(sup2.clientUser, "rods")
+        self.assertEquals(sup2.clientRcatZone, "yoyoyo")
+        self.assertEquals(sup2.relVersion, "irods3.2")
+        self.assertEquals(sup2.apiVersion, "d")
+        self.assertEquals(sup2.option, "hellO")
 
 if __name__ == "__main__":
     unittest.main()
