22dee370205e4c0bc9b2e6c64987c3719c181eee
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-05 12:17:07 -0700

    Fixed ability to initialize session without account parameters

--- irods/session.py
+++ irods/session.py
@@ -23,7 +23,8 @@ class iRODSSession(object):
     def __init__(self, *args, **kwargs):
         self.socket = None
         self.authenticated = False
-        self.configure(*args, **kwargs)
+        if args or kwargs:
+            self.configure(*args, **kwargs)
 
     def configure(self, host=None, port=1247, user=None, zone=None, password=None):
         self.host = host
