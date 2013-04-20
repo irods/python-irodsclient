d6411117f2a73952e6392ef0f2e3043ac49aab4a
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-04-19 22:52:48 -0700

    Added prettytable as a dependency

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 setup(name='pycommands',
       version='0.0',
+      author='Chris LaRose',
+      author_email='cjlarose@iplantcollaborative.org',
       packages=['irods', 'irods.message'],
+      requires=['prettytable==0.7.2']
       )
