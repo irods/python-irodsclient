b1ac97d93f209ef9ac3c3035c5a8f0565b4f01ae
Michael Gatto <mgatto@iplantcollaborative.org>
2013-09-03 11:13:58 -0700

    Run all tests at once, if desired.

---
+++ irods/test/runner.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from unittest import TestLoader, TextTestRunner, TestSuite
+import sys
+import os
+
+"""
+NOTE: "If a test package name (directory with __init__.py) matches the pattern
+       then the package will be checked for a load_tests function. If this
+       exists then it will be called with loader, tests, pattern."
+"""
+
+"""
+Load all tests in the current directory and run them
+"""
+if __name__ == "__main__":
+    # must set the path for the imported tests
+    sys.path.insert(0, os.path.abspath('../..'))
+
+    loader = TestLoader()
+    suite = TestSuite(loader.discover(start_dir='.', pattern='*_test.py',
+                                      top_level_dir="."))
+
+    runner = TextTestRunner(verbosity=2)
+    runner.run(suite)
