ae1021d629fae8579f52ade0a364bb9949738c06
Falmarri <psychicsurgeon@gmail.com>
2014-01-09 14:19:48 -0700

    Thread safe connection pool

--- irods/pool.py
+++ irods/pool.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 
 from irods.connection import Connection
 
@@ -7,20 +8,22 @@ logger = logging.getLogger(__name__)
 class Pool(object):
     def __init__(self, account):
         self.account = account
-
+        self._lock = threading.Lock()
         self.active = set()
         self.idle = set()
         
     def get_connection(self):
-        try:
-            conn = self.idle.pop()
-        except KeyError:
-            conn = Connection(self, self.account)
-        self.active.add(conn)
+        with self._lock:
+            try:
+                conn = self.idle.pop()
+            except KeyError:
+                conn = Connection(self, self.account)
+            self.active.add(conn)
         logger.debug('num active: %d' % len(self.active))
         return conn
 
     def release_connection(self, conn):
-        self.active.remove(conn)
-        self.idle.add(conn)
+        with self._lock:
+            self.active.remove(conn)
+            self.idle.add(conn)
         logger.debug('num idle: %d' % len(self.idle))
