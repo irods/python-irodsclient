1f5511e5afdcb3e8977d6d2918cc82585c2e2251
Low Kian Seong <kianseong@gmail.com>
2014-09-18 04:14:48 +0800

    Update setup.py
    Adding more metadata to `setup.py` for a more complete listing on `pypi`

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     version='0.3',
     author='Chris LaRose',
     author_email='cjlarose@iplantcollaborative.org',
+    description='A python API for iRODS',
+    url='https://github.com/iPlantCollaborativeOpenSource/',
     packages=['irods', 'irods.message', 'irods.resource_manager'],
     install_requires=[
         'PrettyTable>=0.7,<1.0'
