dcd234c166c06bdfc40fa2ef135c2511e0a4e7ac
J. Matt Peterson <jmatt@jmatt.org>
2014-09-17 13:37:19 -0700

    Update version.
    	modified:   AUTHORS
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-irodsclient',
-      version='0.3.1',
+      version='0.4.0',
       author='iPlant Collaborative',
       author_email='atmodevs@gmail.com',
       description='A python API for iRODS',
-      url='https://github.com/iPlantCollaborativeOpenSource/',
+      url='https://github.com/iPlantCollaborativeOpenSource/python-irodsclient',
       packages=find_packages(),
       install_requires=['PrettyTable>=0.7.2'])
