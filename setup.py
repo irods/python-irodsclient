404d866702c752c6bb259807a9f52aeea4bb2070
Antoine de Torcy <adetorcy@email.unc.edu>
2016-08-11 16:07:55 -0400

    [#47] Update package files

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-irodsclient',
-      version='0.5.0',
+      version='0.5.0rc1',
       author='iRODS Consortium',
       author_email='support@irods.org',
       description='A python API for iRODS',
+      license='BSD',
       url='https://github.com/irods/python-irodsclient',
+      keywords='irods',
+      classifiers=[
+                   'License :: OSI Approved :: BSD License',
+                   'Development Status :: 2 - Pre-Alpha',
+                   'Programming Language :: Python :: 2.7',
+                   'Operating System :: POSIX :: Linux',
+                   ],
       packages=find_packages(),
       install_requires=['PrettyTable>=0.7.2'])
