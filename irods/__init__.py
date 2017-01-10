a34ed3732af5a55fdc4a31c6a75676aacb5d6fe5
Antoine de Torcy <adetorcy@email.unc.edu>
2017-01-10 18:18:26 -0500

    [#58] Remove logging and update version

--- irods/__init__.py
+++ irods/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 # iRODS settings
-IRODS_VERSION = {'major': 4, 'minor': 1, 'patchlevel': 6, 'api': 'd'}
+IRODS_VERSION = {'major': 4, 'minor': 2, 'patchlevel': 0, 'api': 'd'}
 
 # Magic Numbers
 MAX_PASSWORD_LENGTH = 50
@@ -11,11 +11,10 @@ MAX_NAME_LEN = MAX_PATH_ALLOWED + 64
 RESPONSE_LEN = 16
 CHALLENGE_LEN = 64
 
-# Logging
-logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
-h = logging.StreamHandler()
-f = logging.Formatter(
-    "%(asctime)s %(name)s-%(levelname)s [%(pathname)s %(lineno)d] %(message)s")
-h.setFormatter(f)
-logger.addHandler(h)
+# logger = logging.getLogger()
+# logger.setLevel(logging.ERROR)
+# h = logging.StreamHandler()
+# f = logging.Formatter(
+#     "%(asctime)s %(name)s-%(levelname)s [%(pathname)s %(lineno)d] %(message)s")
+# h.setFormatter(f)
+# logger.addHandler(h)
