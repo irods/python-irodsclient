4afd78919acfa52dabc30c1faacd7ffce24c3b63
baran <bascibaran@email.arizona.edu>
2017-02-13 12:34:58 -0700

    figured out environmental weirdness

--- irods/session.py
+++ irods/session.py
@@ -8,7 +8,13 @@ from irods.manager.access_manager import AccessManager
 from irods.manager.user_manager import UserManager, UserGroupManager
 from irods.manager.resource_manager import ResourceManager
 from irods.exception import NetworkException
-
+import socket
+from irods.api_number import api_number
+from irods.message import (
+            iRODSMessage, StartupPack, AuthResponse, AuthChallenge,
+                OpenedDataObjRequest, FileSeekResponse, StringStringMap, VersionResponse,
+                )
+from irods.exception import get_exception_by_code, NetworkException
 
 class iRODSSession(object):
 
@@ -43,9 +49,7 @@ class iRODSSession(object):
                   host=None, port=1247, user=None, zone=None,
                   password=None, client_user=None, client_zone=None,
                   server_dn=None, authentication_scheme='password'):
-        account = iRODSAccount(
-            host, int(port), user, zone, authentication_scheme,
-            password, client_user, server_dn, client_zone)
+        account = iRODSAccount(host, int(port), user, zone, authentication_scheme, password, client_user, server_dn, client_zone)
         self.pool = Pool(account)
 
     def query(self, *args):
@@ -56,7 +60,35 @@ class iRODSSession(object):
         # self.port = port
         # self.proxy_user = self.client_user = user
         # self.proxy_zone = self.client_zone = zone
+    
+    def miscsvrinfo(self):
+        # implementing functionality of imiscsvrinfo.
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect((self.account.host, self.account.port))
+        except socket.error:
+            raise NetworkException("Could not connect to specified host and port!")
+        msg = iRODSMessage(msg_type='RODS_API_REQ',msg=None,int_info=api_number['GET_MISC_SVR_INFO_AN'])
+        string = msg.pack()
+        logger.debug(string)
+        try:
+            self.socket.sendall(string)
+        except:
+            logger.error("UNABLE TO SEND MESSAGE, RELEASING CONNECTION FROM POOL")
+            self.pool.release_connection(self.pool.get_connection(),destroy)
+            raise NetworkException("Unable to send message")
+        try:
+            msg = iRODSMEssage.recv(Self.socket)
+        except socket.error:
+            logger.error("Could not receive server response")
+            exit(1)
+        if msg.int_info < 0:
+            raise get_exception_by_code(msg.int_info)
+        print msg
+        return msg
+        
 
+                    
     @property
     def username(self):
         return self.pool.account.client_user
