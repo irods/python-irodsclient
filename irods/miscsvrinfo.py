f810fc2620d2ab9baa2fd609a82ef0a81ceec985
bascibaran <baran.balkan@gmail.com>
2017-03-13 14:32:42 -0700

    finished the unit test, also added API macro to test/config.py

--- irods/miscsvrinfo.py
+++ irods/miscsvrinfo.py
@@ -9,19 +9,21 @@ from irods.exception import NetworkException, get_exception_by_code
 #
 # analogue to icommand imiscsvrinfo. 
 # the object is essentially the struct that the icommand uses to get the info.
-# the different components of imiscsvrinfo's return payload, eg serverType, uptime, etc
+# the different components of imiscsvrinfo's return payload, eg serverType, boot time, etc
 # are attributes of the instance. 
 #
 # example usage: 
-# from irods.miscsvrinfo import miscsvrinfo
+# from irods.miscsvrinfo import iMiscSvrInfo
 # m = iMiscSvrInfo('myirodshost.domain.net')
-# m.uptime     # the uptime in seconds
+# m.serverBootTime     # boot time of the server(seconds since the beginning of epoch)
 # m.serverType # RCAT enabled or disabled
 # and so on. 
 
-# If you wish to update the information, you can call the method
-# m.update() which then updates all the attributes with current information 
 # print m # yields the same output as imiscsvrinfo
+# the property of the object gives us the boot time of the server
+# but the string representation of the object
+# computes and displays the uptime in human readable format, 
+# in its emulation of the output of imiscsvrinfo. 
 
 class iMiscSvrInfo(object):
 
@@ -29,11 +31,11 @@ class iMiscSvrInfo(object):
         self.host=host
         self.port=port
         self.dummycred=""
-        self._rodsZone    = None
-        self._relVersion  = None
-        self._apiVersion  = None
-        self._serverType  = None
-        self._uptime      = None
+        self._rodsZone            = None
+        self._relVersion          = None
+        self._apiVersion          = None
+        self._serverType          = None
+        self._serverBootTime      = None
         self.setInfo()
         return
 
@@ -79,24 +81,21 @@ class iMiscSvrInfo(object):
             root[2].text,
             root[3].text,
             root[4].text,
-            (int(time.time()) - int(root[1].text)))
+            int(root[1].text))
         s.close()
         return
   
-    def setProps(self, st, rv, av, rz, ut):
-        self._serverType = st
-        self._relVersion = rv
-        self._apiVersion = av
-        self._rodsZone   = rz
-        self._uptime     = ut
-
-    def update(self):
-        self.setInfo()
-        return
+    def setProps(self, st, rv, av, rz, bt):
+        self._serverType         = st
+        self._relVersion         = rv
+        self._apiVersion         = av
+        self._rodsZone           = rz
+        self._serverBootTime     = bt
 
     def __str__(self):
-        mins = self.uptime / 60
-        hr = mins/60
+        uptime = (int(time.time()) - self.serverBootTime)
+        mins = uptime / 60
+        hr   = mins/60
         mins = mins%60
         day = hr/24
         hr = hr%24
@@ -116,5 +115,5 @@ class iMiscSvrInfo(object):
     def rodsZone(self):
         return self._rodsZone
     @property
-    def uptime(self):
-        return self._uptime
+    def serverBootTime(self):
+        return self._serverBootTime
--- irods/test/miscsvrinfo_test.py
+++ irods/test/miscsvrinfo_test.py
@@ -6,24 +6,32 @@ import os
 import sys
 import unittest
 import irods.test.config  as config
+import time
 
 from irods.miscsvrinfo import iMiscSvrInfo
 
 class TestMiscSvrInfo(unittest.TestCase):
-    ###
+    
     def setUp(self):
         self.m = iMiscSvrInfo(config.IRODS_SERVER_HOST)
 
-    def testObjectFields(self):
+    def testServerType(self):
         self.assertTrue(self.m.serverType == "RCAT_ENABLED")
+    
+    def testRelVersion(self):
         relString = "rods" + ".".join([str(x) for x in config.IRODS_SERVER_VERSION])
         self.assertTrue(self.m.relVersion == relString)
-        self.assertTrue(self.m.rodsZone   == config.IRODS_SERVER_ZONE)
-        
-        #test uptime?
-        #test apiVersion?
 
-        #test update function??
+    def testRodsZone(self):
+        self.assertTrue(self.m.rodsZone   == config.IRODS_SERVER_ZONE)
+
+    def testServerBootTime(self):
+        curr =  int(time.time())
+        self.assertTrue(self.m.serverBootTime > 0 and self.m.serverBootTime < curr)
+
+    def testApiVersion(self):
+        self.assertTrue(self.m.apiVersion == config.IRODS_API_VERSION)
+
 
 if __name__ == '__main__':
     sys.path.insert(0, os.path.abspath('../..'))
