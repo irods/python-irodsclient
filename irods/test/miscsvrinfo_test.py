ffe95d8206218bf6d32bfcb027b77c9674ae1cde
bascibaran <baran.balkan@gmail.com>
2017-03-12 19:47:27 -0700

    1st pass miscsvrinfo unit test. doesnt cover uptime, apiversion, or update

--- irods/test/miscsvrinfo_test.py
+++ irods/test/miscsvrinfo_test.py
@@ -8,5 +8,24 @@ import unittest
 import irods.test.config  as config
 import irods.test.helpers as helpers
 
+from irods.miscsvrinfo import iMiscSvrInfo
+
 class TestMiscSvrInfo(unittest.TestCase):
     ###
+    def setUp(self):
+        self.m = iMiscSvrInfo(config.IRODS_SERVER_HOST)
+
+    def testObjectFields:
+        self.assertTrue(self.m.serverType == "RCAT_ENABLED")
+        relString = "rods" + ".".join(map(str,config.IRODS_SERVER_VERSION))
+        self.assertTrue(self.m.relVersion == relString)
+        self.assertTrue(self.m.rodsZone   == config.IRODS_SERVER_ZONE)
+        
+        #test uptime?
+        #test apiVersion?
+
+        #test update function??
+
+if __name__ == '__main__':
+    sys.path.insert(0, os.path.abspath('../..'))
+    unittest.main()
