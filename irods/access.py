54e5212f28fa73125c79b9465c2a488fda9314b2
Antoine de Torcy <adetorcy@email.unc.edu>
2017-07-21 11:39:29 -0400

    [#85] Set default empty username in iRODSAccess

--- irods/access.py
+++ irods/access.py
@@ -1,6 +1,6 @@
 class iRODSAccess(object):
 
-    def __init__(self, access_name, path, user_name, user_zone=''):
+    def __init__(self, access_name, path, user_name='', user_zone=''):
         self.access_name = access_name
         self.path = path
         self.user_name = user_name
