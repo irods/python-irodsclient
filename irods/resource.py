696856c02d08511bdc958be3f15c04c2e689658e
Antoine de Torcy <adetorcy@email.unc.edu>
2018-02-11 15:00:19 +0000

    [#111] Lazy load resource children

--- irods/resource.py
+++ irods/resource.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import sys
 from irods.models import Resource
 import six
 
@@ -33,17 +32,50 @@ class iRODSResource(object):
                     try:
                         setattr(self, attr, result[value])
                     except KeyError:
-                        # backward compatibility with pre iRODS 4
-                        sys.exc_clear()
+                        # backward compatibility with older schema versions
+                        pass
 
         self._meta = None
+
 
     @property
     def context_fields(self):
         return dict(pair.split("=") for pair in self.context.split(";"))
 
+
+    @property
+    def children(self):
+        try:
+            return self._children
+        except AttributeError:
+            # the children have not yet been resolved
+            session = self.manager.sess
+            version = session.server_version
+
+            if version >= (4,2,0):
+                # iRODS 4.2+: find parent by resource id
+                parent = self.id
+            elif version >= (4,0,0):
+                # iRODS 4.0/4.1: find parent by resource name
+                parent = self.name
+            else:
+                raise RuntimeError('Resource composition not supported')
+
+            # query for children and cache results
+            query = session.query(Resource).filter(Resource.parent == '{}'.format(parent))
+            self._children = [self.__class__(self.manager, res) for res in query]
+
+            return self._children
+
+
+    @children.setter
+    def children(self, children):
+        pass
+
+
     def __repr__(self):
         return "<iRODSResource {id} {name} {type}>".format(**vars(self))
+
 
     def remove(self, test=False):
         self.manager.remove(self.name, test)
