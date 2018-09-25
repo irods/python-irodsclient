ddb9cee2982999bdf762db6704e2e42cddb0d339
Alan King <alanking@renci.org>
2018-09-25 15:07:53 -0400

    [#137] Add test for filename with ampersand

--- irods/test/data_obj_test.py
+++ irods/test/data_obj_test.py
@@ -959,6 +959,39 @@ class TestDataObjOps(unittest.TestCase):
         # delete file
         os.remove(test_file)
 
+    def test_register_with_xml_special_chars(self):
+        # skip if server is remote
+        if self.sess.host not in ('localhost', socket.gethostname()):
+            self.skipTest('Requires access to server-side file(s)')
+
+        # test vars
+        test_dir = '/tmp'
+        filename = '''aaa'"<&test&>"'_file'''
+        test_file = os.path.join(test_dir, filename)
+        collection = self.coll.path
+        obj_path = '{collection}/{filename}'.format(**locals())
+
+        # make random 4K binary file
+        with open(test_file, 'wb') as f:
+            f.write(os.urandom(1024 * 4))
+
+        # register file in test collection
+        print('registering [' + obj_path + ']')
+        self.sess.data_objects.register(test_file, obj_path)
+
+        # confirm object presence
+        print('getting [' + obj_path + ']')
+        obj = self.sess.data_objects.get(obj_path)
+
+        # in a real use case we would likely
+        # want to leave the physical file on disk
+        print('unregistering [' + obj.path + ']')
+        obj.unregister()
+
+        # delete file
+        os.remove(test_file)
+
+
 if __name__ == '__main__':
     # let the tests find the parent irods lib
     sys.path.insert(0, os.path.abspath('../..'))
