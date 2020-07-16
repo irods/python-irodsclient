7f52a8b3c4e3633161c9bede7f1818312ee33da7
d-w-moore <dmoore@renci.org>
2020-07-16 16:06:14 -0400

    [#207] add raw-acl permissions getter
    Disabled by default; to enable, follow the usage
    
        session.permissions.get( object, report_raw_acls = True )

--- irods/manager/access_manager.py
+++ irods/manager/access_manager.py
@@ -4,20 +4,38 @@ from os.path import basename, dirname
 from irods.manager import Manager
 from irods.api_number import api_number
 from irods.message import ModAclRequest, iRODSMessage
-from irods.data_object import iRODSDataObject
+from irods.data_object import ( iRODSDataObject, irods_dirname, irods_basename )
 from irods.collection import iRODSCollection
-from irods.models import (
-    DataObject, Collection, User, DataAccess, CollectionAccess, CollectionUser)
+from irods.models import ( DataObject, Collection, User, CollectionUser,
+                           DataAccess, CollectionAccess )
 from irods.access import iRODSAccess
+from irods.column import In
+from irods.user import iRODSUser
 
+import six
 import logging
 
 logger = logging.getLogger(__name__)
 
+def users_by_ids(session,ids=()):
+    try:
+        ids=list(iter(ids))
+    except TypeError:
+        if type(ids) in (str,) + six.integer_types: ids=int(ids)
+        else: raise
+    cond = () if not ids \
+           else (In(User.id,list(map(int,ids))),) if len(ids)>1 \
+           else (User.id == int(ids[0]),)
+    return [ iRODSUser(session.users,i)
+             for i in session.query(User.id,User.name,User.type,User.zone).filter(*cond) ]
 
 class AccessManager(Manager):
 
-    def get(self, target):
+    def get(self, target, report_raw_acls = False, **kw):
+
+        if report_raw_acls:
+            return self.__get_raw(target, **kw)  # prefer a behavior consistent  with 'ils -A`
+
         # different query whether target is an object or a collection
         if type(target) == iRODSDataObject:
             access_type = DataAccess
@@ -44,6 +62,56 @@ class AccessManager(Manager):
             path=target.path,
             user_zone=row[user_type.zone]
         ) for row in results]
+
+    def coll_access_query(self,path):
+        return self.sess.query(Collection, CollectionAccess).filter(Collection.name == path)
+
+    def data_access_query(self,path):
+        cn = irods_dirname(path)
+        dn = irods_basename(path)
+        return self.sess.query(DataObject, DataAccess).filter( Collection.name == cn, DataObject.name == dn )
+
+    def __get_raw(self, target, **kw):
+
+        ### sample usage: ###
+        #
+        #  user_id_list = []  # simply to store the user id's from the discovered ACL's
+        #  session.permissions.get( data_or_coll_target, report_raw_acls = True,
+        #                                                acl_users = user_id_list,
+        #                                                acl_users_transform = lambda u: u.id)
+        #
+        # -> returns list of iRODSAccess objects mapping one-to-one with ACL's stored in the catalog
+
+        users_out = kw.pop( 'acl_users', None )
+        T = kw.pop( 'acl_users_transform', lambda value : value )
+
+        # different choice of query based on whether target is an object or a collection
+        if isinstance(target, iRODSDataObject):
+            access_column = DataAccess
+            query_func    = self.data_access_query
+
+        elif isinstance(target, iRODSCollection):
+            access_column = CollectionAccess
+            query_func    = self.coll_access_query
+        else:
+            raise TypeError
+
+        rows  = [ r for r in query_func(target.path) ]
+        userids = set( r[access_column.user_id] for r in rows )
+
+        user_lookup = { j.id:j for j in users_by_ids(self.sess, userids) }
+
+        if isinstance(users_out, dict):     users_out.update (user_lookup)
+        elif isinstance (users_out, list):  users_out += [T(v) for v in user_lookup.values()]
+        elif isinstance (users_out, set):   users_out |= set(T(v) for v in user_lookup.values())
+        elif users_out is None: pass
+        else:                   raise TypeError
+
+        acls = [ iRODSAccess ( r[access_column.name],
+                               target.path,
+                               user_lookup[r[access_column.user_id]].name,
+                               user_lookup[r[access_column.user_id]].zone  ) for r in rows ]
+        return acls
 
     def set(self, acl, recursive=False, admin=False):
         prefix = 'admin:' if admin else ''
--- irods/test/access_test.py
+++ irods/test/access_test.py
@@ -4,7 +4,10 @@ import os
 import sys
 import unittest
 from irods.access import iRODSAccess
+from irods.user import iRODSUser
+from irods.models import User
 import irods.test.helpers as helpers
+from irods.column import In
 
 
 class TestAccess(unittest.TestCase):
@@ -21,6 +24,7 @@ class TestAccess(unittest.TestCase):
         '''
         self.coll.remove(recurse=True, force=True)
         self.sess.cleanup()
+
 
     def test_list_acl(self):
         # test args
@@ -113,6 +117,48 @@ class TestAccess(unittest.TestCase):
         # reset permission to own
         acl1 = iRODSAccess('own', coll.path, user.name, user.zone)
         self.sess.permissions.set(acl1)
+
+    mapping = dict( [ (i,i) for i in ('modify object', 'read object', 'own') ] +
+                    [ ('write','modify object') , ('read', 'read object') ]
+                  )
+
+    @classmethod
+    def perms_lists_symm_diff ( cls, a_iter, b_iter ):
+        fields = lambda perm: (cls.mapping[perm.access_name], perm.user_name, perm.user_zone)
+        A = set (map(fields,a_iter))
+        B = set (map(fields,b_iter))
+        return (A-B) | (B-A)
+
+    def test_raw_acls__207(self):
+        data = helpers.make_object(self.sess,"/".join((self.coll_path,"test_obj")))
+        eg = eu = fg = fu = None
+        try:
+            eg = self.sess.user_groups.create ('egrp')
+            eu = self.sess.users.create ('edith','rodsuser')
+            eg.addmember(eu.name,eu.zone)
+            fg = self.sess.user_groups.create ('fgrp')
+            fu = self.sess.users.create ('frank','rodsuser')
+            fg.addmember(fu.name,fu.zone)
+            my_ownership = set([('own', self.sess.username, self.sess.zone)])
+            #--collection--
+            perms1data = [ iRODSAccess ('write',self.coll_path, eg.name, self.sess.zone),
+                           iRODSAccess ('read', self.coll_path, fu.name, self.sess.zone)
+                         ]
+            for perm in perms1data: self.sess.permissions.set ( perm )
+            p1 = self.sess.permissions.get ( self.coll, report_raw_acls = True)
+            self.assertEqual(self.perms_lists_symm_diff( perms1data, p1 ), my_ownership)
+            #--data object--
+            perms2data = [ iRODSAccess ('write',data.path, fg.name, self.sess.zone),
+                           iRODSAccess ('read', data.path, eu.name, self.sess.zone)
+                         ]
+            for perm in perms2data: self.sess.permissions.set ( perm )
+            p2 = self.sess.permissions.get ( data, report_raw_acls = True)
+            self.assertEqual(self.perms_lists_symm_diff( perms2data, p2 ), my_ownership)
+        finally:
+            ids_for_delete = [ u.id for u in (fu,fg,eu,eg) if u is not None ]
+            for u in [ iRODSUser(self.sess.users,row)
+                       for row in self.sess.query(User).filter(In(User.id, ids_for_delete)) ]:
+                u.remove()
 
 
 if __name__ == '__main__':
