567ac18cf449ea6207a562ffa25220992f38c6c5
d-w-moore <dmoore@renci.org>
2020-08-28 11:37:57 +0000

    doc string for irods.pool.Pool

--- irods/pool.py
+++ irods/pool.py
@@ -14,13 +14,18 @@ DEFAULT_CLIENT_NAME='python-irodsclient'
 
 class Pool(object):
 
-    def __init__(self, account, name = ''):
+    def __init__(self, account, client_name = ''):
+        '''
+        Pool( account , client_name='' )
+        Create an iRODS connection pool; 'account' is an irods.account.iRODSAccount instance
+        and 'client_name' specifies an application name as it should appear in an 'ips' listing.
+        '''
         self.account = account
         self._lock = threading.RLock()
         self.active = set()
         self.idle = set()
         self.connection_timeout = DEFAULT_CONNECTION_TIMEOUT
-        self.client_name = (name  or  os.environ.get('spOption','')  or  DEFAULT_CLIENT_NAME)
+        self.client_name = (client_name  or  os.environ.get('spOption','')  or  DEFAULT_CLIENT_NAME)
 
     def get_connection(self):
         with self._lock:
