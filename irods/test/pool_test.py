4f3034af6f91aaabbb833385a1a00a3277b6ed7f
d-w-moore <dmoore@renci.org>
2021-02-08 18:08:15 -0500

    [221] make connection pool tests work under CI

--- irods/test/pool_test.py
+++ irods/test/pool_test.py
@@ -5,19 +5,46 @@ import os
 import re
 import sys
 import time
+import json
 import unittest
 import irods.test.helpers as helpers
 
 #  Regular expression to match common synonyms for localhost.
 #
+
 LOCALHOST_REGEX = re.compile(r"""^(127(\.\d+){1,3}|[0:]+1|(.*-)?localhost(\.\w+)?)$""",re.IGNORECASE)
+USE_ONLY_LOCALHOST = False
+
 
 class TestPool(unittest.TestCase):
 
+    config_extension = ".json"
+    test_extension = ""
+    preferred_parameters = {}
+
+    @classmethod
+    def setUpClass(cls):              # generate test env files using connect data from ~/.irods environment
+        if USE_ONLY_LOCALHOST: return
+        Nonlocal_Ext = ".test"
+        with helpers.make_session() as session:
+            cls.preferred_parameters = { 'irods_host':session.host,
+                                         'irods_port':session.port,
+                                         'irods_user_name':session.username,
+                                         'irods_zone_name':session.zone }
+            test_configs_dir = os.path.join(irods_test_path(),"test-data")
+            for config in [os.path.join(test_configs_dir,f) for f in os.listdir(test_configs_dir)
+                           if f.endswith(cls.config_extension)]:
+                with open(config,"r") as in_, open(config + Nonlocal_Ext,"w") as out_:
+                    cf = json.load(in_)
+                    cf.update(cls.preferred_parameters)
+                    json.dump(cf, out_,indent=4)
+            cls.test_extension = Nonlocal_Ext
+
+
     def setUp(self):
         self.sess = helpers.make_session(
-            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment.json"))
-        if not LOCALHOST_REGEX.match (self.sess.host):
+            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment.json" + self.test_extension))
+        if USE_ONLY_LOCALHOST and not LOCALHOST_REGEX.match (self.sess.host):
             self.skipTest('for non-local server')
 
     def tearDown(self):
@@ -217,21 +244,21 @@ class TestPool(unittest.TestCase):
 
     def test_get_connection_refresh_time_none_existant_env_file(self):
         connection_refresh_time = self.sess.get_connection_refresh_time(
-            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment_non_existant.json"))
+            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment_non_existant.json" + self.test_extension))
         self.assertEqual(connection_refresh_time, -1)
 
     def test_get_connection_refresh_time_no_connection_refresh_field(self):
         connection_refresh_time = self.sess.get_connection_refresh_time(
-            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment_no_refresh_field.json"))
+            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment_no_refresh_field.json" + self.test_extension))
         self.assertEqual(connection_refresh_time, -1)
 
     def test_get_connection_refresh_time_negative_connection_refresh_field(self):
         connection_refresh_time = self.sess.get_connection_refresh_time(
-            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment_negative_refresh_field.json"))
+            irods_env_file=os.path.join(irods_test_path(),"test-data","irods_environment_negative_refresh_field.json" + self.test_extension))
         self.assertEqual(connection_refresh_time, -1)
 
     def test_get_connection_refresh_time(self):
-        default_path = os.path.join (irods_test_path(),"test-data","irods_environment.json")
+        default_path = os.path.join (irods_test_path(),"test-data","irods_environment.json" + self.test_extension)
         connection_refresh_time = self.sess.get_connection_refresh_time(irods_env_file=default_path)
         self.assertEqual(connection_refresh_time, 3)
 
