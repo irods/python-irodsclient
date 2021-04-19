13abbd88a275f14cfb5b25165712b602e90dce3f
d-w-moore <dmoore@renci.org>
2021-04-19 12:52:24 +0000

    [221] DEST RESC for make_compound test

--- irods/test/admin_test.py
+++ irods/test/admin_test.py
@@ -8,6 +8,7 @@ from irods.exception import UserDoesNotExist, ResourceDoesNotExist
 from irods.session import iRODSSession
 from irods.resource import iRODSResource
 import irods.test.helpers as helpers
+import irods.keywords as kw
 
 
 class TestAdmin(unittest.TestCase):
@@ -157,7 +158,7 @@ class TestAdmin(unittest.TestCase):
         obj = session.data_objects.create(obj_path, comp.name)
 
         # write to object
-        with obj.open('w+') as obj_desc:
+        with obj.open('w+',**{kw.DEST_RESC_NAME_KW:comp.name}) as obj_desc:
             obj_desc.write(dummy_str)
 
         # refresh object
