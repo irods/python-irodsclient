10ab2d4a91b7ef1a1d0c9f42abe8bb18d5e1917a
d-w-moore <dmoore@renci.org>
2021-04-26 13:12:21 -0400

    [221] client dot infrastructure

---
+++ irods_consortium_continuous_integration_test_module.py
@@ -0,0 +1,25 @@
+import json
+import sys
+
+def run (CI):
+
+    final_config = CI.store_config(
+        {
+            "yaml_substitutions": {       # -> written to ".env"
+                "python_version" : "3",
+                "client_os_generic": "ubuntu",
+                "client_os_image": "ubuntu:18.04"
+            },
+            "container_environments": {
+                "client-runner" : {       # -> written to "client-runner.env"
+                    "TESTS_TO_RUN": ""    # run test subset, e.g. "irods.test.data_obj_test"
+                }
+
+            }
+        }
+    )
+
+    print ('----------\nconfig after CI modify pass\n----------',file=sys.stderr)
+    print(json.dumps(final_config,indent=4),file=sys.stderr)
+
+    return CI.run_and_wait_on_client_exit ()
