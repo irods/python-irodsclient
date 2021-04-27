b31cfdf62823fb9c16b4a34b33b9083b6f3b2e62
d-w-moore <dmoore@renci.org>
2021-04-27 07:40:15 -0400

    unbuffered output better to show test progress

--- run_python_tests.sh
+++ run_python_tests.sh
@@ -2,6 +2,9 @@
 
 set -o pipefail
 cd repo/irods/test 
+
+export PYTHONUNBUFFERED="Y"
+
 if [ -z "${TESTS_TO_RUN}" ] ; then
     python${PY_N} runner.py 2>&1 | tee ${LOG_OUTPUT_DIR}/prc_test_logs.txt
 else 
