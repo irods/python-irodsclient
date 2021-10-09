b3bdcbb1544444b6e063086c71f941c15d2461a3
Kory Draughn <korydraughn@renci.org>
2021-10-09 17:37:56 -0400

    [#299] Added support for deleting tickets.

--- irods/ticket.py
+++ irods/ticket.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import
+
+from irods.api_number import api_number
+from irods.message import iRODSMessage, TicketAdminRequest
+
 import random
 import string
-from irods.api_number import api_number
-from irods.message import (
-    iRODSMessage, TicketAdminRequest)
-
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 class Ticket(object):
     def __init__(self, session, ticket=None):
@@ -23,10 +22,8 @@ class Ticket(object):
     def ticket(self):
         return self._ticket
 
-
     def generate(self, length=15):
         return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(length))
-
 
     def supply(self):
         message_body = TicketAdminRequest("session", self.ticket)
@@ -36,9 +33,16 @@ class Ticket(object):
             conn.send(message)
             response = conn.recv()
 
-
     def issue(self, permission, target):
         message_body = TicketAdminRequest("create", self.ticket, permission, target)
+        message = iRODSMessage("RODS_API_REQ", msg=message_body, int_info=api_number['TICKET_ADMIN_AN'])
+
+        with self.session.pool.get_connection() as conn:
+            conn.send(message)
+            response = conn.recv()
+
+    def delete(self):
+        message_body = TicketAdminRequest("delete", self.ticket)
         message = iRODSMessage("RODS_API_REQ", msg=message_body, int_info=api_number['TICKET_ADMIN_AN'])
 
         with self.session.pool.get_connection() as conn:
