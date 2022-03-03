0c7aa37838475658d47a5ec4416e5f7da841dac8
d-w-moore <dmoore@renci.org>
2022-03-03 15:16:22 -0500

    [#43][#328] reasonable indentation

--- irods/user.py
+++ irods/user.py
@@ -46,8 +46,8 @@ class iRODSUser(object):
 
     def modify_password(self, old_value, new_value, modify_irods_authentication_file = False):
         self.manager.modify_password(old_value,
-                                         new_value,
-                                         modify_irods_authentication_file = modify_irods_authentication_file)
+                                     new_value,
+                                     modify_irods_authentication_file = modify_irods_authentication_file)
 
     def modify(self, *args, **kwargs):
         self.manager.modify(self.name, *args, **kwargs)
