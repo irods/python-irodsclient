17e34f4acec1527647e1a86f4006ee3e23e8d0b3
d-w-moore <dmoore@renci.org>
2024-01-30 11:07:48 -0500

    [_502]{_SQ] experimental test harness using containers
    testuser universal for all containers
    
    login_auth*.py adjustments
    
    [__502] scriptdir
    
    [__502] patch for consistency in starting
    
    document SKIP_IINIT_FOR_PASSWORD
    
    "funcs" include file renamed to "test_support_functions"
    
    remove fail.sh to scripts dir
    
    extra time parameter to age_out_pam_password
    
    SKIP_IINIT_FOR_PASSWORD option for pam user env setup
    
    add mtime_and_content
    
    add tests 001 thru 009 to test params
    
    add irods_server_version to test_support_functions
    
    can now give exact filename of dockerfiles to build
    
    firm up package defs; allow run under podman
    
    correct test009 name
    
    bats tests - IRODS version macros, params
    
    README.txt
    
    version-string-driven building of python interpreter builder image
    
    script to start server under iRODS 5

---
+++ irods/test/harness/start_postgresql_and_irods.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+service postgresql start
+x=${DB_WAIT_SEC:-20}
+while [ $x -ge 0 ] && { ! $SUDO su - postgres -c "psql -c '\l' >/dev/null 2>&1" || x=""; }
+do
+  [ -z "$x" ] && break
+  echo >&2 "$((x--)) secs til database timeout"; sleep 1
+done
+[ -z "$x" ] || { echo >&2 "Error -- database didn't start" ; exit 1; }
+if ! id -u irods >/dev/null 2>&1 ; then
+    /install.sh --w=create-db 0
+    VERSION_file=$(ls /var/lib/irods/{VERSION,version}.json.dist 2>/dev/null)
+    IRODS_VSN=$(jq -r '.irods_version' $VERSION_file) /install.sh 5
+fi
+su - irods -c '~/irodsctl restart'
+IRODS_WAIT_SEC=20
+x=$IRODS_WAIT_SEC
+SLEEPTIME=""
+while [ $((x--)) -gt 0 ]; do
+  sleep $((SLEEPTIME+0))
+  pgrep irodsServer
+  STATUS=$?
+  [ $STATUS -eq 0 ] && break
+  SLEEPTIME=1
+done
+echo "($STATUS)" >/tmp/irods_status
+[ $STATUS -eq 0 ] || exit 125
+tail -f /dev/null
