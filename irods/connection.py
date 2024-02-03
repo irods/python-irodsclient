651f603da5e92dff71cdedcf551ff7ccc7ca80ea
d-w-moore <dmoore@renci.org>
2024-02-03 01:07:36 -0500

    [_517] allow generating a pam-password based .irodsA if not pre-existing

--- irods/connection.py
+++ irods/connection.py
@@ -532,8 +532,9 @@ class Connection(object):
         self._login_native(password = auth_out.result_)
 
         # Store new password in .irodsA if requested.
-        if self.account._auth_file and cfg.legacy_auth.pam.store_password_to_environment:
-            with open(self.account._auth_file,'w') as f:
+        auth_file = (self.account._auth_file or self.account.derived_auth_file)
+        if auth_file and cfg.legacy_auth.pam.store_password_to_environment:
+            with open(auth_file,'w') as f:
                 f.write(obf.encode(auth_out.result_))
                 logger.debug('new PAM pw write succeeded')
 
