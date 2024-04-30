f1f7392eb867a9c0ab3ba8b9220f258ad2e80936
Mustafa Dikmen <mustafa.dikmen@kuleuven.be>
2024-04-30 18:10:13 +0200

    Check name and value type before length

--- irods/manager/metadata_manager.py
+++ irods/manager/metadata_manager.py
@@ -96,8 +96,9 @@ class MetadataManager(Manager):
 
     def add(self, model_cls, path, meta, **opts):
         # Avoid sending request with empty argument(s)
-        if not(len(path) and len(meta.name) and len(meta.value)):
-            raise ValueError('Empty value in ' + repr(meta))
+        if type(meta.name) == 'str' and type(meta.value) == 'str':
+            if not(len(path) and len(meta.name) and len(meta.value)):
+                raise ValueError('Empty value in ' + repr(meta))
 
         resource_type = self._model_class_to_resource_type(model_cls)
         message_body = MetadataRequest(
