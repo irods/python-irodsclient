90f8e9889580755ce40037e99de96381aadd198f
d-w-moore <dmoore@renci.org>
2024-12-06 11:16:43 -0500

    [_635] Free fcns now have keyword parameter to dictate overwriting of old .irodsA
    That parameter is named "overwrite" and defaults to True.
    
    Also: we add a helper function _open_for_protected_content, a context manager which
    encapsulates the opening for write, writing, and closing of a file with protected
    content -- only readable by the owner. Thus is uses a umask of 077 octal by default).

--- irods/client_init.py
+++ irods/client_init.py
@@ -1,33 +1,62 @@
-from irods import (env_filename_from_keyword_args, derived_auth_filename)
-import irods.client_configuration as cfg
-import irods.password_obfuscation as obf
-import irods.helpers as h
+import contextlib
 import getpass
 import os
 import sys
 
-def write_native_credentials_to_secrets_file(password, **kw):
-    env_file = env_filename_from_keyword_args(kw)
-    auth_file = derived_auth_filename(env_file)
-    old_mask = None
+from irods import (env_filename_from_keyword_args, derived_auth_filename)
+import irods.client_configuration as cfg
+import irods.password_obfuscation as obf
+import irods.helpers as h
+
+@contextlib.contextmanager
+def _open_file_for_protected_contents(file_path,*arg,**kw):
+    f = old_mask = None
     try:
         old_mask = os.umask(0o77)
-        open(auth_file,'w').write(obf.encode(password))
+        f = open(file_path,*arg,**kw)
+        yield f
     finally:
+        if f is not None:
+            f.close()
         if old_mask is not None:
             os.umask(old_mask)
-            
-    return True
 
-def write_pam_credentials_to_secrets_file( password ,**kw):
+def write_native_credentials_to_secrets_file(password, overwrite = True, **kw):
+    """Write the credentials to an .irodsA file that will enable logging in with native authentication
+       using the given cleartext password.
+
+       If overwrite is False, an already existing .irodsA will not be overwritten.
+
+       Returns: True if credentials were written to the .irodsA file.
+    """
+    env_file = env_filename_from_keyword_args(kw)
+    auth_file = derived_auth_filename(env_file)
+    old_mask = os.umask(0o77)
+    if overwrite or not os.path.exists(auth_file):
+        with _open_file_for_protected_contents(auth_file,'w',**kw) as irodsA:
+            irodsA.write(obf.encode(password))
+        return True
+    return False
+
+def write_pam_credentials_to_secrets_file(password, overwrite = True, **kw):
+    """Write the credentials to an .irodsA file that will enable logging in with PAM authentication
+       using the given cleartext password.
+
+       If overwrite is False, an already existing .irodsA will not be overwritten.
+
+       Returns: True if credentials were written to the .irodsA file.
+    """
     s = h.make_session()
     s.pool.account.password = password
     with cfg.loadlines( [dict(setting='legacy_auth.pam.password_for_auto_renew',value=None),
                          dict(setting='legacy_auth.pam.store_password_to_environment',value=False)] ):
         to_encode = s.pam_pw_negotiated
-    if to_encode:
-        open(s.pool.account.derived_auth_file,'w').write(obf.encode(to_encode[0]))
-        return True
+    auth_file_path = s.pool.account.derived_auth_file
+    if overwrite or not os.path.exists(auth_file):
+        if to_encode:
+            with _open_file_for_protected_contents(auth_file,'w',**kw) as irodsA:
+                irodsA.write(obf.encode(to_encode[0]))
+            return True
     return False
 
 if __name__ == '__main__':
