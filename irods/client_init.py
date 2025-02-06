0be8141b5fe77dc5015d592f1296de9fb281a94a
d-w-moore <dmoore@renci.org>
2025-02-06 03:23:38 -0500

    fix pam free fn

--- irods/client_init.py
+++ irods/client_init.py
@@ -56,12 +56,17 @@ def write_native_credentials_to_secrets_file(password, overwrite=True, **kw):
 ## (Here's a start:)
 def write_pam_irodsA_file(password, overwrite=True, **kw):
     import irods.auth, irods.helpers
+    env_file = env_filename_from_keyword_args(kw)
+    auth_file = derived_auth_filename(env_file)
+    if not auth_file:
+        return
     ses = h.make_session(**kw)
     ses.set_auth_option_for_scheme('pam_password', irods.auth.FORCE_PASSWORD_PROMPT, io.StringIO(password))
+
     L = []
     ses.set_auth_option_for_scheme('pam_password', irods.auth.CLIENT_GET_REQUEST_RESULT, L)
-    ses.collections.get(irods.helpers.home_collection(ses))
-    _write_encoded_auth_value(auth_file, password, overwrite)
+    ses.collections.get(irods.helpers.home_collection(ses))  # get a collection; more importantly, make a socket connection
+    _write_encoded_auth_value(auth_file, L[0], overwrite)
 
 
 def write_pam_credentials_to_secrets_file(password, overwrite=True, **kw):
