723a6f021e6610dbf0658eb0bb9bd5fd2a8f916c
d-w-moore <dmoore@renci.org>
2025-09-16 07:33:29 -0400

    branch builds for irods 4 and 5

--- docker-testing/start_containers.sh
+++ docker-testing/start_containers.sh
@@ -4,8 +4,16 @@ set -e
 # This script is launched on the docker host.
 
 usage() {
-  echo >&2 "usage: $0 irods_version python_version"; exit 1;
+  echo >&2 "usage: $0 [-b "<docker compose build args>"] irods_version python_version"; exit 1;
 }
+
+SHELL_DOCKER_COMPOSE_BUILD_ARGS=""
+
+if [ $1 = "-b" ]
+then
+    SHELL_DOCKER_COMPOSE_BUILD_ARGS=$2
+    shift 2
+fi
 
 if [ $# -eq 2 ]; then
     IRODS_VERSION=$1
@@ -17,16 +25,17 @@ else
     usage
 fi
 
-REPO_ROOT=$(realpath ..)
+shift $#
 
-shift 2
 [ -n "$PYTHON_VERSION" -a -n "$IRODS_VERSION" ] || {
      usage
 }
+
 IRODS_MAJOR=$(sed -e 's/\..*//' <<<"$IRODS_VERSION")
 
 DIR=$(dirname "$0")
 cd "${DIR}"
+REPO_ROOT=$(realpath ..)
 
 echo "\
 repo_external=\"${REPO_ROOT}\"
@@ -34,5 +43,6 @@ python_version=\"${PYTHON_VERSION}\"
 irods_version=\"${IRODS_VERSION}\"
 irods_major=\"${IRODS_MAJOR}\"" >.env
 
-docker compose -f harness-docker-compose-irods-${IRODS_MAJOR}.yml build $*
-docker compose -f harness-docker-compose-irods-${IRODS_MAJOR}.yml up -d
+docker compose -f harness-docker-compose-irods-${IRODS_MAJOR}.yml build $SHELL_DOCKER_COMPOSE_BUILD_ARGS
+##dwm
+#docker compose -f harness-docker-compose-irods-${IRODS_MAJOR}.yml up -d
