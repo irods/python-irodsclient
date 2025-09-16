40653c3113f6f49f792207c9968a8abb4b8b5160
d-w-moore <dmoore@renci.org>
2025-09-16 07:03:13 -0400

    add "irods_major" as docker compose param

--- docker-testing/start_containers.sh
+++ docker-testing/start_containers.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 set -e
-IRODS_VERSION=$1
-PYTHON_VERSION=$2
+
+# This script is launched on the docker host.
+
+usage() {
+  echo >&2 "usage: $0 irods_version python_version"; exit 1;
+}
+
+if [ $# -eq 2 ]; then
+    IRODS_VERSION=$1
+    PYTHON_VERSION=$2
+elif [ $# -eq 1 ]; then
+    IRODS_VERSION=4.3.4
+    PYTHON_VERSION=$1
+else
+    usage
+fi
+
+REPO_ROOT=$(realpath ..)
+
 shift 2
 [ -n "$PYTHON_VERSION" -a -n "$IRODS_VERSION" ] || {
-  echo >&2 "usage: $0 irods_version python_version"; exit 1;
+     usage
 }
 IRODS_MAJOR=$(sed -e 's/\..*//' <<<"$IRODS_VERSION")
 
@@ -12,8 +29,10 @@ DIR=$(dirname "$0")
 cd "${DIR}"
 
 echo "\
-python_version=${PYTHON_VERSION}
-irods_version=${IRODS_VERSION}" >.env
+repo_external=\"${REPO_ROOT}\"
+python_version=\"${PYTHON_VERSION}\"
+irods_version=\"${IRODS_VERSION}\"
+irods_major=\"${IRODS_MAJOR}\"" >.env
 
 docker compose -f harness-docker-compose-irods-${IRODS_MAJOR}.yml build $*
 docker compose -f harness-docker-compose-irods-${IRODS_MAJOR}.yml up -d
