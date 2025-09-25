9076727532a2607da921e8d070e209b3e83c10e2
d-w-moore <dmoore@renci.org>
2025-09-25 15:19:32 -0400

    healthcheck replaces oneshot mechanism

--- docker-testing/run_tests.sh
+++ docker-testing/run_tests.sh
@@ -6,7 +6,10 @@ if [ -z "$PYTHON" ]; then
 fi
 DIR=$(dirname "$0")
 cd "$DIR"
-$PYTHON ./recv_oneshot -h irods-catalog-provider -p 8888 -t 360
+
+## Replacing server ready notify mechanism with Docker healthcheck
+#$PYTHON ./recv_oneshot -h irods-catalog-provider -p 8888 -t 360
+
 REPO="$(./print_repo_root_location)"
 #$PYTHON -m pip install "$REPO[tests]"
 
