98903fd75990ced0764f3cfbc0c5e1d217bef8a1
d-w-moore <dmoore@renci.org>
2025-09-26 09:13:30 -0400

    correct iRODS version detect for server start

--- irods/test/harness/start_postgresql_and_irods.sh
+++ irods/test/harness/start_postgresql_and_irods.sh
@@ -7,15 +7,18 @@ do
   echo >&2 "$((x--)) secs til database timeout"; sleep 1
 done
 [ -z "$x" ] || { echo >&2 "Error -- database didn't start" ; exit 1; }
+VERSION_file=$(ls /var/lib/irods/{VERSION,version}.json.dist 2>/dev/null)
+export IRODS_VSN=$(jq -r '.irods_version' $VERSION_file)
 if ! id -u irods >/dev/null 2>&1 ; then
     /install.sh --w=create-db 0
-    VERSION_file=$(ls /var/lib/irods/{VERSION,version}.json.dist 2>/dev/null)
-    IRODS_VSN=$(jq -r '.irods_version' $VERSION_file) /install.sh 5
+    /install.sh 5
 fi
-if ! [ $(sed 's!\..*!!'<<<$IRODS_PACKAGE_VERSION) -lt 5 ]; then 
-    /manage_irods5_procs start
-else
+IRODS_VSN_MAJOR=$(sed 's!\..*!!'<<<$IRODS_VSN)
+unset IRODS_VSN
+if [ $IRODS_VSN_MAJOR -lt 5 ]; then 
     su - irods -c '~/irodsctl restart'
+else
+    /manage_irods5_procs start
 fi
 IRODS_WAIT_SEC=20
 x=$IRODS_WAIT_SEC
